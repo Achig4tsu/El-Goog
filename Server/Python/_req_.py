483982146aad9bf23b886b4401d3f3d66e549c60
S4riel <silva.florian83@gmail.com>
2024-02-26 16:08:22 +0100

    Amelioration

---
+++ Server/Python/_req_.py
@@ -0,0 +1,17 @@
+"""
+
+Module de requêtes.
+
+Fait par : - SILVA Florian
+           - BOUT Gabriel
+           
+Version : 1.0
+
+"""
+
+import requests, time
+
+class Req :
+    
+    def __init__(self, url:str , id:int) -> None:
+        
---
+++ Server/Scripts/Crawler.py
@@ -0,0 +1,9 @@
+"""
+
+    Code source crawler.
+    Fait par Florian SILVA et Gabriel BOUT
+    
+    Version : 1.0
+
+"""
+
